81d8b519cf643d2438787b4636c3f81a5dfc3b52
Soufiane Boursen <dev.sboursen@gmail.com>
2022-09-03 11:19:43 +0100

    Hundle requests other than GET

--- pages/api/staticard.ts
+++ pages/api/staticard.ts
@@ -2,15 +2,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import createCard from '../../components/card/card';
 
-
+type Data =
+  | string
+  | {
+      message: string;
+    };
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<string>,
+  res: NextApiResponse<Data>,
 ) {
   if (req.method === 'GET') {
-    const body = createCard(200, 200)
+    const body = createCard(200, 200);
     res.status(200);
     res.setHeader('Content-Type', 'image/svg+xml');
     res.send(body);
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
