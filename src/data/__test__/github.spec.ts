4810603d8bc57f1af34e3a7a70355538fc9d940d
Soufiane Boursen <dev.sboursen@gmail.com>
2022-10-12 11:42:14 +0100

    Mock Axios response
    - Tests should not deal with an external Api's response. Otherwise, we get a lot of false negatives.

--- src/data/__test__/github.spec.ts
+++ src/data/__test__/github.spec.ts
@@ -1,4 +1,8 @@
 import getUserContributions from '../github';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('getUserContributions', () => {
   beforeAll(() => {
@@ -8,6 +12,32 @@ describe('getUserContributions', () => {
     jest.setTimeout(5000);
   });
   it('should resolve for the default userHandle and year', async () => {
+    mockedAxios.request.mockResolvedValueOnce({
+      data: {
+        data: {
+          user: null,
+        },
+        errors: [
+          {
+            type: 'NOT_FOUND',
+            path: ['user'],
+            locations: [
+              {
+                line: 2,
+                column: 3,
+              },
+            ],
+            message:
+              "Could not resolve to a User with the login of 'Sboufsfrsen'.",
+          },
+        ],
+      },
+      status: 200,
+      statusText: 'Ok',
+      headers: {},
+      config: {},
+    });
+
     const year = 2022;
     const userHandle = 'Sboursen';
     const data = await getUserContributions(userHandle, year);
@@ -18,6 +48,31 @@ describe('getUserContributions', () => {
   });
 
   it('should return empty user data for non existent user', async () => {
+    mockedAxios.request.mockResolvedValueOnce({
+      data: {
+        data: {
+          user: null,
+        },
+        errors: [
+          {
+            type: 'NOT_FOUND',
+            path: ['user'],
+            locations: [
+              {
+                line: 2,
+                column: 3,
+              },
+            ],
+            message:
+              "Could not resolve to a User with the login of 'Sbouaslkdfgbnowgrsen'.",
+          },
+        ],
+      },
+      status: 200,
+      statusText: 'Ok',
+      headers: {},
+      config: {},
+    });
     const year = 2022;
     const userHandle = 'Sbouaslkdfgbnowgrsen';
     const data = await getUserContributions(userHandle, year);
